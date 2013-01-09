0a716122f2b4da79d19f85c493d13f1d5f3ad5a0
hashishi <bcah1220050@gn.iwasaki.ac.jp>
2013-01-09 11:30:20 +0900

    ５回目のコミット
    １つ目の数字を２つ目の数字で割った余りを表示させるようにしました

--- CL_K01_1/CL_K01_1.c
+++ CL_K01_1/CL_K01_1.c
@@ -2,7 +2,7 @@
 /*作成者：1T3(29)　橋本顕成*/
 /*課題:CL_K01_1*/
 void main(void){
-	int sum,a,b,c;
+	int sum,a,b,c,d;
 	double ave;
 	printf("1つ目の数字を入力してください");
 	scanf("%d",&a);
@@ -11,7 +11,9 @@ void main(void){
 	sum = a + b;
 	c = a * b;
 	ave = (a + b)/2;
+	d = a % b;
 	printf("%dと%dを足した値は%dです。\n",a,b,sum);
 	printf("%dと%dをかけた値は%dです。\n",a,b,c);
-	printf("%dと%dをかけた値は%fです。\n",a,b,ave);
+	printf("%dと%dの平均の値は%fです。\n",a,b,ave);
+	printf("%dを%dで割った余りは%dです。\n",a,b,d);
 }
