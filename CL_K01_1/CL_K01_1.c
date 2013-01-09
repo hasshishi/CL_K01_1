08d8c65a176481d33fb266761004d18df2887c7c
hashishi <bcah1220050@gn.iwasaki.ac.jp>
2013-01-09 11:12:23 +0900

    ２回目のコミット
    数字を入力できるようにしました

--- CL_K01_1/CL_K01_1.c
+++ CL_K01_1/CL_K01_1.c
@@ -2,7 +2,11 @@
 /*作成者：1T3(29)　橋本顕成*/
 /*課題:CL_K01_1*/
 void main(void){
-	int sum;
-	sum = 15+ 37;
-	printf("15に37を足した値は%dです。\n",sum);
+	int sum,a,b;
+	printf("1つ目の数字を入力してください");
+	scanf("%d",&a);
+	printf("2つ目の数字を入力してください");
+	scanf("%d",&b);
+	sum = a + b;
+	printf("%dと%dを足した値は%dです。\n",a,b,sum);
 }
