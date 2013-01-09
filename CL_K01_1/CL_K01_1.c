99d59cfec06e5a6c24d808361ac4795e4a5553ee
hashishi <bcah1220050@gn.iwasaki.ac.jp>
2013-01-09 11:21:47 +0900

    ３回目のコミット
    かけた値も表示させました

--- CL_K01_1/CL_K01_1.c
+++ CL_K01_1/CL_K01_1.c
@@ -2,11 +2,13 @@
 /*作成者：1T3(29)　橋本顕成*/
 /*課題:CL_K01_1*/
 void main(void){
-	int sum,a,b;
+	int sum,a,b,c;
 	printf("1つ目の数字を入力してください");
 	scanf("%d",&a);
 	printf("2つ目の数字を入力してください");
 	scanf("%d",&b);
 	sum = a + b;
+	c = a * b;
 	printf("%dと%dを足した値は%dです。\n",a,b,sum);
+	printf("%dと%dをかけた値は%dです。\n",a,b,c);
 }
