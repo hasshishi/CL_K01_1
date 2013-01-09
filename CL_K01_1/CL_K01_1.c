853533f3251cb76eaf084f426f278e0fa934c300
hashishi <bcah1220050@gn.iwasaki.ac.jp>
2013-01-09 11:25:10 +0900

    ４回目のコミット
    平均も表示させるようにしました

--- CL_K01_1/CL_K01_1.c
+++ CL_K01_1/CL_K01_1.c
@@ -3,12 +3,15 @@
 /*課題:CL_K01_1*/
 void main(void){
 	int sum,a,b,c;
+	double ave;
 	printf("1つ目の数字を入力してください");
 	scanf("%d",&a);
 	printf("2つ目の数字を入力してください");
 	scanf("%d",&b);
 	sum = a + b;
 	c = a * b;
+	ave = (a + b)/2;
 	printf("%dと%dを足した値は%dです。\n",a,b,sum);
 	printf("%dと%dをかけた値は%dです。\n",a,b,c);
+	printf("%dと%dをかけた値は%fです。\n",a,b,ave);
 }
